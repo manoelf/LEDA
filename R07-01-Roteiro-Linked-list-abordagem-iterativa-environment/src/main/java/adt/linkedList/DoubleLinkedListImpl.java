a62cda16ad560ed4f3ed6f06961b1813b10a3979
manoelf <jose.ferreira@ccc.ufcg.edu.br>
2016-08-03 21:34:53 -0300

    Roteiro 07 implementando . . .

--- R07-01-Roteiro-Linked-list-abordagem-iterativa-environment/src/main/java/adt/linkedList/DoubleLinkedListImpl.java
+++ R07-01-Roteiro-Linked-list-abordagem-iterativa-environment/src/main/java/adt/linkedList/DoubleLinkedListImpl.java
@@ -6,9 +6,17 @@ public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements
 	protected DoubleLinkedListNode<T> last;
 	
 	@Override
-	public void insertFirst(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+	public void insertFirst(T element) {//element, next, previous
+		
+		DoubleLinkedListNode<T> newHead = new DoubleLinkedListNode<>(element, (DoubleLinkedListNode<T>)getHead(), (DoubleLinkedListNode<T>)nil);
+		SingleLinkedListNode<T> nil = new SingleLinkedListNode<>(null, newHead);
+		
+		if (isEmpty()) {
+			this.last = (DoubleLinkedListNode<T>) nil;
+			
+		}
+		
+		
 	}
 
 	@Override

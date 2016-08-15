23bce541a185d823a187c3171310ab0921a71e17
manoelf <jose.ferreira@ccc.ufcg.edu.br>
2016-08-15 14:41:56 -0300

    Roteiro 10

--- R10-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
+++ R10-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
@@ -1,27 +1,22 @@
 package adt.hashtable.open;
 
-import adt.hashtable.AbstractHashtable;
-import adt.hashtable.Util;
 import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
 import adt.hashtable.hashfunction.HashFunctionLinearProbing;
 
-public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends AbstractHashtableOpenAddress<T> {
+public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends
+		AbstractHashtableOpenAddress<T> {
 
-	public HashtableOpenAddressLinearProbingImpl(int size, HashFunctionClosedAddressMethod method) {
+	public HashtableOpenAddressLinearProbingImpl(int size,
+			HashFunctionClosedAddressMethod method) {
 		super(size);
 		hashFunction = new HashFunctionLinearProbing<T>(size, method);
+		this.initiateInternalTable(size);
 	}
 
 	@Override
 	public void insert(T element) {
-		if (element != null) {
-			if (isFull()) {
-				
-			}
-			
-
-		
-		}
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
 	}
 
 	@Override
@@ -41,5 +36,5 @@ public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends A
 		// TODO Auto-generated method stub
 		throw new UnsupportedOperationException("Not implemented yet!");
 	}
-	
+
 }
--- R10-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressQuadraticProbingImpl.java
+++ R10-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressQuadraticProbingImpl.java
@@ -1,27 +1,22 @@
 package adt.hashtable.open;
 
-import adt.hashtable.AbstractHashtable;
-import adt.hashtable.Util;
 import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
-import adt.hashtable.hashfunction.HashFunctionLinearProbing;
+import adt.hashtable.hashfunction.HashFunctionQuadraticProbing;
 
-public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends AbstractHashtableOpenAddress<T> {
+public class HashtableOpenAddressQuadraticProbingImpl<T extends Storable>
+		extends AbstractHashtableOpenAddress<T> {
 
-	public HashtableOpenAddressLinearProbingImpl(int size, HashFunctionClosedAddressMethod method) {
+	public HashtableOpenAddressQuadraticProbingImpl(int size,
+			HashFunctionClosedAddressMethod method, int c1, int c2) {
 		super(size);
-		hashFunction = new HashFunctionLinearProbing<T>(size, method);
+		hashFunction = new HashFunctionQuadraticProbing<T>(size, method, c1, c2);
+		this.initiateInternalTable(size);
 	}
 
 	@Override
 	public void insert(T element) {
-		if (element != null) {
-			if (isFull()) {
-				
-			}
-			
-
-		
-		}
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
 	}
 
 	@Override
@@ -41,5 +36,4 @@ public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends A
 		// TODO Auto-generated method stub
 		throw new UnsupportedOperationException("Not implemented yet!");
 	}
-	
 }

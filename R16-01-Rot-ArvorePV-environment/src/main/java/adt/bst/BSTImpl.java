a30512cd252eed49b6335be51a1637d466c7b6d5
manoelf <jose.ferreira@ccc.ufcg.edu.br>
2016-09-22 02:42:41 -0300

    Roteiro16 em desenvolvimento

--- R16-01-Rot-ArvorePV-environment/src/main/java/adt/bst/BSTImpl.java
+++ R16-01-Rot-ArvorePV-environment/src/main/java/adt/bst/BSTImpl.java
@@ -1,5 +1,7 @@
 package adt.bst;
 
+import adt.bt.BTNode;
+
 public class BSTImpl<T extends Comparable<T>> implements BST<T> {
 
 	protected BSTNode<T> root;
@@ -19,68 +21,320 @@ public class BSTImpl<T extends Comparable<T>> implements BST<T> {
 
 	@Override
 	public int height() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return height(this.root);
+	}
+
+	public int height(BSTNode<T> node) {
+		int resultLeft = -1;
+		int resultRight = -1;
+
+		if (!node.isEmpty()) {
+			resultLeft = 1 + height((BSTNode<T>) node.getLeft());
+			resultRight = 1 + height((BSTNode<T>) node.getRight());
+		}
+		if (resultLeft > resultRight) {
+			return resultLeft;
+		} else {
+			return resultRight;
+		}
 	}
 
 	@Override
 	public BSTNode<T> search(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (isEmpty() ||  element == null) {
+			return new BSTNode<T>();
+		} else {
+			return search(this.root, element);
+		}
+	}
+
+	private BSTNode<T> search(BSTNode<T> node, T element) {
+		if (!node.isEmpty()) {
+			if (node.getData().equals(element)) {
+				return node;
+			} else if (node.getData().compareTo(element) > 0) {
+				return search((BSTNode<T>) node.getLeft(), element);
+			} else {
+				return search((BSTNode<T>) node.getRight(), element);
+			}
+		}
+		return node;
 	}
 
 	@Override
 	public void insert(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (element != null) {
+			insert(this.root, element);
+		}
+	}
+
+	protected BSTNode<T> insert(BSTNode<T> node, T element) {
+		if (node.isEmpty()) {
+
+			node.setData(element);
+			node.setLeft(new BSTNode<T>());
+			node.getLeft().setParent(node);
+			node.setRight(new BSTNode<T>());
+			node.getRight().setParent(node);
+			return node;
+		} else if (node.getData().compareTo(element) > 0) {
+			return insert((BSTNode<T>) node.getLeft(), element);
+		} else if (node.getData().compareTo(element) < 0) {
+			return insert((BSTNode<T>) node.getRight(), element);
+		}
+		return node;
 	}
 
 	@Override
 	public BSTNode<T> maximum() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (isEmpty()) {
+			return null;
+		} else {
+			return maximum(this.root);
+		}
+	}
+
+	private BSTNode<T> maximum(BSTNode<T> node) {
+		if (node.getRight().isEmpty()) {
+			return node;
+		} else {
+			return maximum((BSTNode<T>) node.getRight());
+		}
 	}
 
 	@Override
 	public BSTNode<T> minimum() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (isEmpty()) {
+			return null;
+		} else {
+			return minimum(this.root);
+		}
+	}
+
+	private BSTNode<T> minimum(BTNode<T> node) {
+		if (node.getLeft().isEmpty()) {
+			return (BSTNode<T>) node;
+		} else {
+			return minimum(node.getLeft());
+		}
 	}
 
 	@Override
 	public BSTNode<T> sucessor(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		BSTNode<T> node = search(element);
+		if (element != null && !node.isEmpty()) {
+			if (!node.getRight().isEmpty()) {
+				return minimum(node.getRight());
+			} else {
+				return sucessor(node, (BSTNode<T>) node.getParent());
+			}
+		} else {
+			return null;
+		}
+	}
+	private BSTNode<T> sucessor(BSTNode<T> node) {
+		if (!node.isEmpty()) {
+			if (!node.getRight().isEmpty()) {
+				return minimum(node.getRight());
+			} else {
+				return sucessor(node, (BSTNode<T>) node.getParent());
+			}
+		} else {
+			return null;
+		}
+	}
+ 
+	private BSTNode<T> sucessor(BSTNode<T> node, BSTNode<T> parent) {
+		if (parent == null) {
+			return null;
+		} else if (parent.getData().compareTo(node.getData()) > 0) {
+			return (BSTNode<T>) parent;
+		} else {
+			return sucessor(node, (BSTNode<T>) parent.getParent());
+		}
 	}
 
 	@Override
 	public BSTNode<T> predecessor(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		BSTNode<T> node = search(element);
+		if (element == null || node.isEmpty()) {
+			return null;
+		} else {
+			if (!node.getLeft().isEmpty()) {
+				return maximum((BSTNode<T>) node.getLeft());
+			} else {
+				return predecessor(node, node.getParent());
+			}
+		}
+	}
+
+	private BSTNode<T> predecessor(BSTNode<T> node, BTNode<T> parent) {
+		if (parent == null) {
+			return null;
+		} else if (parent.getData().compareTo(node.getData()) < 0) {
+			return (BSTNode<T>) parent;
+		} else {
+			return predecessor(node, parent.getParent());
+		}
 	}
 
 	@Override
 	public void remove(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		BSTNode<T> node = this.search(element);
+		if (element != null && !node.isEmpty()) {
+			remove(node);
+		}
+	}
+
+	protected BTNode<T> remove(BSTNode<T> node) {
+		// FOLHA
+		if (node.isLeaf()) {
+			return removeLeaf(node);
+			// APENAS UM FILHO
+		} else if (singleChild(node)) {
+			// APENAS FILHO A ESQUERDA
+			if (node.getRight().isEmpty()) {
+				return removeLeftNode(node);
+				// APENAS FILHO A DIREITA
+			} else {
+				// ROOT COM FILHO APENAS A DIREITA
+				return removeRight(node);
+			}
+			// NODE TEM DOIS FILHOS
+		} else {
+			return removeNodeTwoChild(node);
+		}
+	}
+
+	private boolean singleChild(BTNode<T> node) {
+		if (node.isEmpty()) {
+			return false;
+		} else if (node.getLeft().isEmpty() && !node.getRight().isEmpty()) {
+			return true;
+		} else if (!node.getLeft().isEmpty() && node.getRight().isEmpty()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean isRoot(BTNode<T> node) {
+		return node.equals(this.root);
+	}
+
+	private BTNode<T> removeLeaf(BTNode<T> node) {
+		node.setData(null);
+		node.setLeft(null);
+		node.setRight(null);
+		return node;
+	}
+
+	private BTNode<T> removeLeftNode(BTNode<T> node) {
+		// ROOT COM FILHO APENAS A ESQUERDA
+		if (isRoot(node)) {
+			removeRoot(node);
+			// NAO Eh ROOT
+		} else {
+			node.getLeft().setParent(node.getParent());
+			// NODE Eh ESQUERDA DO SEU PARENT
+			if (node.getParent().getLeft().equals(node)) {
+				node.getParent().setLeft(node.getLeft());
+				// NODE Eh DIREITA DE SEU PARENT
+			} else {
+				node.getParent().setRight(node.getLeft());
+			}
+		}
+		return node;
+	}
+
+	private BTNode<T> removeRight(BTNode<T> node) {
+		if (isRoot(node)) {
+			removeRoot(node);
+			// NAO Eh ROOT
+		} else {
+			node.getRight().setParent(node.getParent());
+			// NODE Eh ESQUERDA DE SEU PARENT
+			if (node.getParent().getLeft().equals(node)) {
+				node.getParent().setLeft(node.getRight());
+				// NODE Eh DIREITA DE SEU PARENT
+			} else {
+				node.getParent().setRight(node.getRight());
+			}
+		}
+		return node;
+	}
+
+	private BTNode<T> removeRoot(BTNode<T> node) {
+		// APENAS FILHO A ESQUERDA
+		if (node.getRight().isEmpty()) {
+			this.root = (BSTNode<T>) node.getLeft();
+			node.getLeft().setParent(node.getParent());
+			node.setLeft(null);
+		} else {
+			this.root = (BSTNode<T>) node.getRight();
+			node.getRight().setParent(node.getParent());
+			node.setRight(null);
+		}
+		return node;
+	}
+
+	private BTNode<T> removeNodeTwoChild(BTNode<T> node) {
+		BSTNode<T> sucessor = sucessor((BSTNode<T>)node);
+		T sucessorData = sucessor.getData();
+		sucessor.setData(node.getData());
+		node.setData(sucessorData);
+		return remove(sucessor);
 	}
 
 	@Override
 	public T[] preOrder() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		@SuppressWarnings("unchecked")
+		T[] array = (T[]) new Comparable[this.size()];
+		preOrder(this.root, array, 0);
+		return array;
+	}
+
+	private int preOrder(BTNode<T> node, T[] array, int index) {
+		if (!node.isEmpty()) {
+			array[index] = node.getData();
+			index = preOrder(node.getLeft(), array, ++index);
+			index = preOrder(node.getRight(), array, index);
+		}
+		return index;
 	}
 
 	@Override
 	public T[] order() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		@SuppressWarnings("unchecked")
+		T[] array = (T[]) new Comparable[this.size()];
+		order(this.root, array, 0);
+		return array;
+	}
+
+	private int order(BSTNode<T> node, T[] array, int index) {
+		if (!node.isEmpty()) {
+			index = order((BSTNode<T>) node.getLeft(), array, index);
+			array[index++] = node.getData();
+			index = order((BSTNode<T>) node.getRight(), array, index);
+		}
+		return index;
 	}
 
 	@Override
 	public T[] postOrder() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		@SuppressWarnings("unchecked")
+		T[] array = (T[]) new Comparable[this.size()];
+		postOrder(this.root, array, 0);
+		return array;
+	}
+
+	private int postOrder(BSTNode<T> node, T[] array, int index) {
+		if (!node.isEmpty()) {
+			index = postOrder((BSTNode<T>) node.getLeft(), array, index);
+			index = postOrder((BSTNode<T>) node.getRight(), array, index);
+			array[index++] = node.getData();
+		}
+		return index;
 	}
 
 	/**
--- R16-01-Rot-ArvorePV-environment/src/main/java/adt/rbtree/RBTreeImpl.java
+++ R16-01-Rot-ArvorePV-environment/src/main/java/adt/rbtree/RBTreeImpl.java
@@ -1,23 +1,30 @@
 package adt.rbtree;
 
 import adt.bst.BSTImpl;
+import adt.bst.BSTNode;
+import adt.bt.BTNode;
+import adt.bt.Util;
 import adt.rbtree.RBNode.Colour;
 
-public class RBTreeImpl<T extends Comparable<T>> extends BSTImpl<T>
-		implements RBTree<T> {
+public class RBTreeImpl<T extends Comparable<T>> extends BSTImpl<T> implements RBTree<T> {
 
 	public RBTreeImpl() {
 		this.root = new RBNode<T>();
 	}
 
 	protected int blackHeight() {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return blackHeight(this.root);
+	}
+
+	private int blackHeight(BSTNode<T> node) {
+		if (!isEmpty()) {
+			return 1 + blackHeight((BSTNode<T>) node.getLeft());
+		}
+		return 0;
 	}
 
 	protected boolean verifyProperties() {
-		boolean resp = verifyNodesColour() && verifyNILNodeColour()
-				&& verifyRootColour() && verifyChildrenOfRedNodes()
+		boolean resp = verifyNodesColour() && verifyNILNodeColour() && verifyRootColour() && verifyChildrenOfRedNodes()
 				&& verifyBlackHeight();
 
 		return resp;
@@ -51,8 +58,32 @@ public class RBTreeImpl<T extends Comparable<T>> extends BSTImpl<T>
 	 * be BLACK.
 	 */
 	private boolean verifyChildrenOfRedNodes() {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return verifyChildrenOfRedNodes((RBNode<T>) this.root);
+	}
+
+	private boolean verifyChildrenOfRedNodes(RBNode<T> node) {
+		if (!node.isEmpty()) {
+			if (isRed(node)) {
+				if (isBlack((RBNode<T>) node.getLeft()) && isBlack((RBNode<T>) node.getRight())) {
+					return verifyChildrenOfRedNodes((RBNode<T>) node.getLeft())
+							&& verifyChildrenOfRedNodes((RBNode<T>) node.getRight());
+				} else {
+					return false;
+				}
+			} else {
+				return verifyChildrenOfRedNodes((RBNode<T>) node.getLeft())
+						&& verifyChildrenOfRedNodes((RBNode<T>) node.getRight());
+			}
+		}
+		return true;
+	}
+
+	private boolean isRed(RBNode<T> node) {
+		return node.getColour().equals(Colour.RED);
+	}
+
+	private boolean isBlack(RBNode<T> btNode) {
+		return btNode.getColour().equals(Colour.BLACK);
 	}
 
 	/**
@@ -60,14 +91,26 @@ public class RBTreeImpl<T extends Comparable<T>> extends BSTImpl<T>
 	 * returns an exception if the black heights are different.
 	 */
 	private boolean verifyBlackHeight() {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return verifyBlackHeight(this.root.getLeft()) == verifyBlackHeight(this.root.getRight());
+	}
+
+	private int verifyBlackHeight(BTNode<T> node) {
+		if (!node.isEmpty()) {
+			if (isBlack((RBNode<T>) node)) {
+				return 1 + verifyBlackHeight(node.getLeft()) + verifyBlackHeight(node.getRight());
+			} else {
+				return verifyBlackHeight(node.getLeft()) + verifyBlackHeight(node.getRight());
+			}
+		}
+		return 0;
 	}
 
 	@Override
 	public void insert(T value) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+	
+	private void insert(RBNode<T> node, T value) {
+		//TODO
 	}
 
 	@Override
@@ -78,27 +121,99 @@ public class RBTreeImpl<T extends Comparable<T>> extends BSTImpl<T>
 
 	// FIXUP methods
 	protected void fixUpCase1(RBNode<T> node) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (node != null) {
+			if (isRoot(node)) {
+				node.setColour(Colour.BLACK);
+			} else {
+				fixUpCase2(node);
+			}
+		}
 	}
 
 	protected void fixUpCase2(RBNode<T> node) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (node.getParent() != null) {
+			if (isRed((RBNode<T>) node.getParent())) {
+				fixUpCase3(node);
+			}
+		}
 	}
 
 	protected void fixUpCase3(RBNode<T> node) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		RBNode<T> uncle = getUncle(node); 
+		if (uncle != null) {
+			if (isRed(uncle)) {
+				uncle.setColour(Colour.BLACK);
+				((RBNode<T>)node.getParent()).setColour(Colour.BLACK);
+				RBNode<T> granFa = getGranFather(node);
+				granFa.setColour(Colour.RED);
+				fixUpCase1(granFa);
+			} else {
+				fixUpCase4(node);
+			}
+		}
 	}
 
 	protected void fixUpCase4(RBNode<T> node) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (isRightChildren(node) && isLeftChildren((BSTNode<T>) node.getParent())) {
+			Util.leftRotation((BSTNode<T>) node.getParent());
+			fixUpCase5((RBNode<T>) node.getRight());
+		} else if (isLeftChildren(node) && isRightChildren((BSTNode<T>) node.getParent())) {
+			Util.rightRotation((BSTNode<T>) node.getParent());
+			fixUpCase5((RBNode<T>) node.getLeft());
+		}
 	}
 
 	protected void fixUpCase5(RBNode<T> node) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		((RBNode<T>)node.getParent()).setColour(Colour.BLACK);
+		RBNode<T> granFa = getGranFather(node);
+		granFa.setColour(Colour.RED);
+		if (isLeftChildren(node)) {
+			Util.rightRotation(granFa);
+		} else {
+			Util.leftRotation(granFa);
+			
+		}
+	
+	}
+	
+	private RBNode<T> getGranFather(BSTNode<T> node) {
+		BSTNode<T> parent = (BSTNode<T>) node.getParent();
+		if (parent != null) {
+			return (RBNode<T>) node.getParent().getParent();
+		} else {
+			return null;
+		}
+	}
+	
+	private boolean isLeftChildren(BSTNode<T> node) {
+		BSTNode<T> parent = (BSTNode<T>) node.getParent();
+		if (parent != null) {
+			if (node.equals(parent.getLeft())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private boolean isRightChildren(BSTNode<T> node) {
+		BSTNode<T> parent = (BSTNode<T>) node.getParent();
+		if (parent != null) {
+			if (node.equals(parent.getRight())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private RBNode<T> getUncle(BTNode<T> node) {
+		BSTNode<T> granFather = getGranFather((BSTNode<T>) node);
+		if (granFather != null) {
+			if (isLeftChildren((BSTNode<T>) node.getParent())) {
+				return (RBNode<T>) granFather.getRight();
+			} else {
+				return (RBNode<T>) granFather.getLeft();
+			}
+		} 
+		return null;
 	}
 }
